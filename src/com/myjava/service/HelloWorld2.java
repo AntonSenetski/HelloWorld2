a7e409279847aa887d4c26c277c9e01005d6e40f
AntonSenetski <smx@live.ru>
2022-11-07 21:59:18 +0200

    hi

---
+++ src/com/myjava/service/HelloWorld2.java
@@ -0,0 +1,9 @@
+package com.myjava.service;
+
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld2" + " By Anthony");
+        System.out.println("Privet");
+    }
+}
